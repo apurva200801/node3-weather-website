ea1287902310ec59559a6dc1d07d39dd8a517a17
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2020-08-25 09:12:45 +0530

    heroku fix

--- web-server/src/index.js
+++ web-server/src/index.js
@@ -4,8 +4,8 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-const app = express()
-cont port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000
 
 const pubilcDirectoryPath = path.join(__dirname, '../public')
 const viewsPath= path.join(__dirname, '../../templates/views')
